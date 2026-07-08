184b093c3575725bf4e5bbf217a6e8f06129da47
karibee <kloico08@gmail.com>
2026-07-08 14:19:27 -0300

    feat: Classe perfil de usuario, relacionamento 1:1

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import ForeignKey
 from database import Base
+from datetime import date
 
 class Produto_orm(Base):
     __tablename__ = "produtos"
@@ -15,8 +16,20 @@ class Produto_orm(Base):
 class Usuario_orm(Base):
     __tablename__ = "users"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    login : Mapped[str] = mapped_column(unique=True, nullable=False)
-    senha : Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(unique=True, nullable=False)
+    senha: Mapped[str] = mapped_column(nullable=False)
 
     produtos: Mapped["Produto_orm"] = relationship(back_populates="dono")
+    perfil: Mapped["Perfil_orm"] = relationship(back_populates="dono", uselist=False)
+
+class Perfil_orm(Base):
+    __tablename__ = "perfis"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    dono_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False, unique=True)
+    cpf: Mapped[int] = mapped_column(unique=True, nullable=False)
+    telefone: Mapped[int] = mapped_column(unique=True, nullable=True)
+    data_nascimento: Mapped[date] = mapped_column(nullable=False)
+
+    dono: Mapped["Usuario_orm"] = relationship(back_populates="perfil")
